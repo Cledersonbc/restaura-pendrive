8633e1e5cdaf3e1a7a4fae027de7f6eb075362ed
cledersonbc <cledersonbc@live.com>
2019-04-01 20:55:56 -0300

    Documentation for components

--- src/br/edu/cleardrive/view/component/ComponentManager.java
+++ src/br/edu/cleardrive/view/component/ComponentManager.java
@@ -12,21 +12,64 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JViewport;
 
-public class ComponentManager {
+/**
+ * ComponentManager is a way to manage all GUI components, like buttons, fields, textarea etc.
+ * This abstraction contains methods that allows you to put, get and remove GUI components to be
+ * accessed by others classes.
+ *
+ * @author Clederson Cruz
+ *
+ * @since 2.0.0
+ */
+public final class ComponentManager {
 	private static Map<String, JComponent> components = new HashMap<String, JComponent>();
-	
+
+	/**
+	 * No instances are required, private constructor.
+	 *
+	 * @throws AssertionError for convenience (all methods are statics and the component
+	 *                         manager are unique)
+	 */
+	private ComponentManager() {
+		throw new AssertionError("You can not instantiate " + this.getClass().getName());
+	}
+
+	/**
+	 * To register a JComponent into ComponentManager, a name and itself are needed.
+	 *
+	 * @param name of component (must be unique)
+	 * @param component itself
+	 */
 	public static void register(String name, JComponent component) {
 		components.put(name, component);
 	}
-	
+
+	/**
+	 * Removes a JComponent from ComponentManager.
+	 *
+	 * @param name of the component
+	 * @return the component removed
+	 */
 	public static JComponent remove(String name) {
 		return components.remove(name);
 	}
-	
+
+	/**
+	 * Returns a JComponent from ComponentManager.
+	 *
+	 * @param name of the component
+	 * @return the component itself
+	 */
 	public static JComponent get(String name) {
 		return components.get(name);
 	}
-	
+
+	/**
+	 * If false, all components in the GUI that contains actions (and that actions are conflicting)
+	 * will be disabled. If true, all components are true again;.
+	 *
+	 * @param value true or false to enable or disable
+	 */
 	public static void enableConflictingButtons(boolean value) {
 		JButton protecBtn = (JButton) get(ComponentName.PROTECT_BUTTON.toString());
 		JButton recoverBtn = (JButton) get(ComponentName.RECOVER_BUTTON.toString());
@@ -34,12 +77,17 @@ public class ComponentManager {
 		JComboBox<String> listedDrives = (JComboBox<String>) get(
 				ComponentName.DRIVE_CHANGED_COMBOBOX.toString()
 		);
-		
+
 		protecBtn.setEnabled(value);
 		recoverBtn.setEnabled(value);
 		listedDrives.setEnabled(value);
 	}
-	
+
+	/**
+	 * This method appends new messages in output view.
+	 *
+	 * @param text to be inserted
+	 */
 	public static void updateViewOutput(String text) {
 		JTextArea outputView = (JTextArea) get(ComponentName.OUTPUT_VIEW_TEXT_AREA.toString());
 		JViewport viewPort = (JViewport) outputView.getParent();
@@ -49,17 +97,28 @@ public class ComponentManager {
 		outputView.setText(outputView.getText() + text + "\n");
 		scrollBar.setValue(scrollBar.getMaximum());
 	}
-	
+
+	/**
+	 * Clears the output view.
+	 */
 	public static void clearViewOutput() {
 		JTextArea outputView = (JTextArea) get(ComponentName.OUTPUT_VIEW_TEXT_AREA.toString());
 		outputView.setText("");
 	}
-	
+
+	/**
+	 * Fill full progress bar to 100%.
+	 */
 	public static void fillFullProgressBar() {
 		JProgressBar progressBar = (JProgressBar) get(ComponentName.STATUS_PROGRESS_BAR.toString());
 		progressBar.setValue(100);
 	}
-	
+
+	/**
+	 * Receives a increment value and increases the progress bar.
+	 *
+	 * @param increment
+	 */
 	public static void incrementProgressBar(int increment) {
 		JProgressBar progressBar = (JProgressBar) ComponentManager.get(
 				ComponentName.STATUS_PROGRESS_BAR.toString()
--- src/br/edu/cleardrive/view/component/ComponentName.java
+++ src/br/edu/cleardrive/view/component/ComponentName.java
@@ -1,5 +1,12 @@
 package br.edu.cleardrive.view.component;
 
+/**
+ * All components names are registered here as an Enum.
+ *
+ * @author Clederson Cruz
+ *
+ * @since 2.0.0
+ */
 public enum ComponentName {
 	ABOUT_BUTTON, RECOVER_BUTTON, PROTECT_BUTTON, STOP_BUTTON,
 	DRIVE_CHANGED_COMBOBOX,
