054f333cf47b4b14e916c7f742b4eb978b1f9b51
cledersonbc <cledersonbc@live.com>
2019-04-01 19:56:05 -0300

    Documentation for Window class

--- src/br/edu/cleardrive/view/Window.java
+++ src/br/edu/cleardrive/view/Window.java
@@ -2,14 +2,30 @@ package br.edu.cleardrive.view;
 
 import javax.swing.JFrame;
 
+/**
+ * The window shows all components inserted in main panel, like buttons, fields etc.
+ * This class is a JFrame that supports all window's configurations.
+ *
+ * @author Clederson Cruz
+ *
+ * @since 1.0.0
+ *
+ */
+
 public class Window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor initializes all window's configurations.
+	 */
 	public Window() {
 		initWindow();
 	}
 
+	/**
+	 * All window's configurations are setting here.
+	 */
 	private void initWindow() {
 		short width = 450;
 		short height = 400;
@@ -18,7 +34,7 @@ public class Window extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.pack();
 		this.setSize(width, height);
-		this.setLocationRelativeTo(null);	
+		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setContentPane( new MainPanel() );
 		this.setVisible(true);
